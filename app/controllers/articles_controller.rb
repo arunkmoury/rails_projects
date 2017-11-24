2d3da011b431337f91e57e27bd01d091ccfae9a0
Arun kumar <arunkmoury@gmail.com>
2017-11-24 17:22:52 +0530

    articles saved to database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
 	def create
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.
+		@article.save
 		redirect_to articles_show(@article)
 	end
 
