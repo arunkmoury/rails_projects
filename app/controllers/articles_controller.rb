b329adce3cdc07b086931e181d7c675b6cd910e5
Arun kumar <arunkmoury@gmail.com>
2017-11-24 17:45:35 +0530

    showing article edited before

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,18 @@ class ArticlesController < ApplicationController
 	def create
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to articles_show(@article)
+		if @article.save
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+		#@article.save
+	end
+
+	def show
+
+		@article=Article.find(params[:id])
+
 	end
 
 	private
